cd06c211b44f5039859de5c7652b7badb219d00f
evaou <speeder528@gmail.com>
2017-03-28 22:18:22 +0800

    Revert "Integrate Hexagon"
    This reverts commit 35db0e71559071621f6c99c7b7aab90ce44d5844.

--- src/main/kotlin/domoya/app/Main.kt
+++ src/main/kotlin/domoya/app/Main.kt
@@ -1,8 +1,7 @@
 package domoya.app
 
-import co.there4.hexagon.web.*
-
-fun main(args: Array<String>) {
-    get("/hello/{name}") { ok("Hello ${request["name"]}!") }
-    run()
+object Main {
+    @JvmStatic fun main(args: Array<String>) {
+        println("Hello Kotlin!")
+    }
 }
