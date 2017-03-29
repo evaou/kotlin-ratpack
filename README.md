2b5d27a0575abe1b104568914fc06988360c56dd
evaou <speeder528@gmail.com>
2017-03-29 21:24:29 +0800

    Integrate ratpack-java plugin

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# kotlin-ratpack
+# Kotlin Ratpack
+- [Run service](#run-service)
+- [View result](#view-result)
+
+### Run service
+    $ gradle run
+
+### View result
+- [http://localhost:5050](http://localhost:5050)
+- [http://localhost:5050/Kotlin](http://localhost:5050/Kotlin)
--- src/main/kotlin/domoya/app/Main.kt
+++ src/main/kotlin/domoya/app/Main.kt
@@ -1,7 +1,12 @@
 package domoya.app
 
-object Main {
-    @JvmStatic fun main(args: Array<String>) {
-        println("Hello Kotlin!")
+import ratpack.server.RatpackServer
+
+fun main(args: Array<String>) {
+    RatpackServer.start { server -> server
+        .handlers { chain -> chain
+            .get { ctx -> ctx.render("Hello World!") }
+            .get(":name") { ctx -> ctx.render("Hello " + ctx.pathTokens["name"] + "!") }
+        }
     }
 }
